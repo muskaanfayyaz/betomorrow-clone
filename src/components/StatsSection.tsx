382400f623710718de23cff85a744e0588556b09
Muskaan Fayyaz <muskaanfayyaz3@gmail.com>
2025-05-25 18:44:46 +0500

    Betomorrow

--- src/components/StatsSection.tsx
+++ src/components/StatsSection.tsx
@@ -5,50 +5,65 @@ const stats = [
     id: 1,
     image: "/project-400.png",
     alt: "400+ projects",
-    text: (
-      <>
-        projects since our<br />creation in 2002
-      </>
-    ),
+    text: "projects since our creation in 2002",
   },
   {
     id: 2,
     image: "/client-97.png",
     alt: "97% clients",
-    text: (
-      <>
-        of our clients are<br />continuing with us
-      </>
-    ),
+    text: "of our clients are continuing with us",
   },
   {
     id: 3,
     image: "/year-10.png",
     alt: "10+ years AI",
-    text: (
-      <>
-        years of research<br />and development<br />in AI
-      </>
-    ),
+    text: "years of research and development in AI",
   },
 ];
 
 export default function StatsSection() {
   return (
-    <section className="w-full py-16">
-      <div className="max-w-6xl mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-28 text-center">
-        {stats.map((stat) => (
-          <div key={stat.id} className="flex flex-col items-center">
+    <section className="w-full py-16 px-4 xl:px-4 text-left">
+      <div className="max-w-6xl xl:max-w-8xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 xl:gap-24">
+        {/* First two items */}
+        {stats.slice(0, 2).map((stat) => (
+          <div
+            key={stat.id}
+            className="flex flex-col items-start md:items-center space-y-4 px-2"
+          >
             <Image
               src={stat.image}
               alt={stat.alt}
               width={200}
               height={80}
-              className="mx-auto"
+              className="self-start md:self-center"
             />
-            <p className="mt-4 md:text-2xl text-[#0a0a0a] text-4xl text-center w-[200px]">{stat.text}</p>
+            <p className="md:text-2xl text-[#0a0a0a] text-xl w-[200px] text-left md:text-center">
+              {stat.text}
+            </p>
           </div>
         ))}
+
+        {/* Third item: centered on md, left on lg */}
+        <div
+          className="
+            flex flex-col items-start space-y-4 px-2
+            md:col-span-2 md:items-center md:justify-center
+            lg:col-span-1 lg:items-start lg:mt-0
+            mt-8
+          "
+        >
+          <Image
+            src={stats[2].image}
+            alt={stats[2].alt}
+            width={200}
+            height={80}
+            className="self-start md:self-center lg:self-start"
+          />
+          <p className="md:text-2xl text-[#0a0a0a] text-xl w-[200px] text-left md:text-center lg:text-left">
+            {stats[2].text}
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -1,10 +1,10 @@
-// components/UseCasesSection.tsx
 import Image from "next/image";
 import { Sparkles } from "lucide-react";
 
 const useCases = [
   {
-    title_Img: "/1st-usecase.png",
+    title:
+      "<span style='font-family: Parabole;'>A</span>n intelligen<span style='font-family: Parabole;'>t</span> cha<span style='font-family: Parabole;'>tb</span>ot <span style='font-family: Parabole;'>p</span>owered <span style='font-family: Parabole;'>b</span>y <span style='font-family: Parabole;'>y</span>o<span style='font-family: Parabole;'>u</span>r data",
     description:
       "Befinormoor has developed a chatbot based on <strong>RAG</strong><br/> technology, which combines the power of <strong>LLM</strong><br/> generation models with advanced information retrieval,<br/> guaranteeing optimized results.",
     image: "/mockup/Mockup_macbook_RAG_02-640.png",
@@ -15,7 +15,7 @@ const useCases = [
     ],
   },
   {
-    title_Img: "/2nd-usecase.png",
+    title: "<span style='font-family: Parabole;'>T</span>oilet Fin<span style='font-family: Parabole;'>d</span>er: user fee<span style='font-family: Parabole;'>db</span>ac<span style='font-family: Parabole;'>k</span> anal<span style='font-family: Parabole;'>y</span>sis",
     description:
       "Discover an innovative solution combining algorithmic<br/> <strong>clustering</strong>, <strong>embeddings</strong> and <strong>generative AI</strong> to structure<br/> user feedback, visualize it and derive actionable insights.",
     image: "/mockup/014_-_Toiletfinder-3840.png",
@@ -26,7 +26,7 @@ const useCases = [
     ],
   },
   {
-    title_Img: "/3rd-usecase.png",
+    title: "Crea<span style='font-family: Parabole;'>t</span>ing a uni<span style='font-family: Parabole;'>v</span>ersal cha<span style='font-family: Parabole;'>t</span>bot <span style='font-family: Parabole;'>t</span>o ha<span style='font-family: Parabole;'>r</span>ness <span style='font-family: Parabole;'>d</span>a<span style='font-family: Parabole;'>t</span>a e<span style='font-family: Parabole;'>f</span>fec<span style='font-family: Parabole;'>t</span>i<span style='font-family: Parabole;'>v</span>ely",
     description:
       "Our AI experts designed an interactive chatbot for<br/> <strong>advanced and intuitive</strong> business data exploration for<br/> one of our customers.",
     image: "/mockup/R-mockups__iPhone_15_Pro-3840.jpg",
@@ -38,7 +38,8 @@ const useCases = [
     ],
   },
   {
-    title_Img: "/4th-usecase.png",
+    title:
+      "<span style='font-family: Parabole;'>A</span>I a<span style='font-family: Parabole;'>t</span> the hear<span style='font-family: Parabole;'>t</span> o<span style='font-family: Parabole;'>f</span> <span style='font-family: Parabole;'>k</span>nowle<span style='font-family: Parabole;'>d</span>ge management an<span style='font-family: Parabole;'>d</span> pro<span style='font-family: Parabole;'>f</span>essional <span style='font-family: Parabole;'>t</span>raining",
     description:
       "Skillagora is the first solution designed to encourage the exchange of skills and knowledge between co-workers, both face-to-face and remotely.",
     image: "/mockup/Mockup-Skillagora-01-3840.png",
@@ -53,41 +54,34 @@ const useCases = [
 
 export default function UseCasesSection() {
   return (
-    <section className="py-16 px-4 sm:px-8 md:px-16 bg-gradient-to-br from-blue-100 to-white relative">
-      <div className="max-w-7xl mx-auto grid gap-16 grid-cols-1 md:grid-cols-2">
+    <section className="py-16 px-4 sm:px-8 md:px-16 relative">
+      <div className="max-w-7xl mx-auto grid gap-16 grid-cols-1 md:grid-cols-2 xl:gap-60">
         {useCases.map((useCase, index) => (
           <div
             key={index}
-            className={`flex flex-col gap-6 ${
+            className={`flex flex-col gap-4 ${
               index % 2 === 0
                 ? "md:justify-self-start"
                 : "md:justify-self-end md:mt-48"
             }`}
           >
             {/* Mockup Image */}
-            <div className="relative w-full aspect-[4/5] sm:aspect-[4/3] lg:aspect-[16/9] rounded-3xl overflow-hidden shadow-lg">
+            <div className="relative w-full h-[24rem] md:h-[25rem] lg:h-[30rem] xl:h-[36rem] rounded-3xl overflow-hidden shadow-lg">
   <Image
     src={useCase.image}
-    alt={useCase.title_Img}
+    alt="Use case image"
     fill
     className="object-cover"
-    sizes="(max-width: 768px) 100vw,
-           (max-width: 1200px) 45vw,
-           40vw"
+    sizes="(max-width: 425px) 100vw, (max-width: 1024px) 80vw, 40vw"
   />
 </div>
 
-            {/* Title Image */}
-            <div className="relative w-full aspect-[4/1] sm:aspect-[3/1] lg:aspect-[5/1] max-w-full">
-              <Image
-                src={useCase.title_Img}
-                alt={useCase.title_Img}
-                fill
-                className="object-contain"
-                sizes="(max-width: 640px) 100vw,
-                       (max-width: 768px) 90vw,
-                       (max-width: 1024px) 45vw,
-                       40vw"
+
+            {/* Title */}
+            <div className="w-full max-w-full mb-3">
+              <h1
+                className="text-[1.3rem] md:text-[1.8rem] lg:text-[2rem] font-normal mb-0.5 text-black leading-tight"
+                dangerouslySetInnerHTML={{ __html: useCase.title }}
               />
             </div>
 
